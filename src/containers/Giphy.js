dbf17d17463d8705413b25cf94f3b71b43bdf75e
Uche Jude <iamuchejude@gmail.com>
2018-11-05 07:04:10 +0100

    ch(setup): Setup project structure and dependencies

---
+++ src/containers/Giphy.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Giphy extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <h1>Giphy</h1>
+    )
+  }
+}
+
+export default Giphy;
